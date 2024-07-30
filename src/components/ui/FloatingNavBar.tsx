8a1f4f9df03f2a2db0933a250661b782ae9d4f80
Froggy-san <medoosama441@yahoo.com>
2024-07-30 23:02:07 +0300

    fixed some styles

--- src/components/ui/FloatingNavBar.tsx
+++ src/components/ui/FloatingNavBar.tsx
@@ -57,7 +57,7 @@ const FloatingNav = ({
           duration: 0.2,
         }}
         className={cn(
-          "flex max-w-fit  fixed  top-16 inset-x-0 mx-auto border  rounded-lg  bg-muted/50 backdrop-blur-xl  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-[5000] px-4 py-2 items-center justify-center space-x-4",
+          "flex max-w-fit  fixed  top-16 inset-x-0 mx-auto border  rounded-lg  bg-muted/50 backdrop-blur-xl  shadow-[0px_2px_3px_-1px_rgba(0,0,0,0.1),0px_1px_0px_0px_rgba(25,28,33,0.02),0px_0px_0px_1px_rgba(25,28,33,0.08)] z-40 px-4 py-2 items-center justify-center space-x-4",
           className
         )}
       >
