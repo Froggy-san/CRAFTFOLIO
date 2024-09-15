2a6b02b2f6a052de488622844bbe7d6a5f2f57f7
Froggy-san <medoosama441@yahoo.com>
2024-09-15 04:06:51 +0300

    Added the feature to view the profile picture of a project's owner in the projectView component.

--- src/features/projectView/PosterInfo.tsx
+++ src/features/projectView/PosterInfo.tsx
@@ -1,9 +1,10 @@
+import ImageView from "@/components/shared/ImageView";
 import TooltipComp from "@/components/shared/TooltipComp";
 import { Avatar, AvatarFallback, AvatarImage } from "@/components/ui/avatar";
 import { User } from "@/types/types";
 import { calcHowManyDaysAgo } from "@/utils/helper";
 import { format } from "date-fns";
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 
 const PosterInfo = ({
@@ -15,28 +16,40 @@ const PosterInfo = ({
   postDate: string;
   className?: string;
 }) => {
+  const [viewedImage, setViewedImage] = useState<string | null>(null);
+
   return (
-    <Link
-      to={`/user/${poster.userId}`}
-      className={`flex flex-1 gap-2 ${className || ""}`}
-    >
-      <Avatar className=" w-12 h-12">
-        <AvatarImage src={poster.avatar} alt={poster.email} />
+    <div className="flex gap-2">
+      <Avatar
+        onClick={() => setViewedImage(poster.avatar)}
+        className="h-12 w-12 cursor-pointer transition-all duration-200 hover:opacity-95 hover:grayscale-[15%]"
+      >
+        <AvatarImage
+          src={poster.avatar}
+          alt={poster.email}
+          className="object-cover"
+        />
         <AvatarFallback>{poster.email}</AvatarFallback>
       </Avatar>
-      <div>
-        <TooltipComp toolTipText={poster.username}>
-          <p className=" line-clamp-1 text-lg font-semibold tracking-wide">
-            {poster.username}
-          </p>
-        </TooltipComp>
-        <TooltipComp toolTipText={`Posted: ${calcHowManyDaysAgo(postDate)}`}>
-          <p className=" text-xs text-foreground/60">
-            {format(new Date(postDate), "LLLL/dd/yyyy")}
-          </p>
-        </TooltipComp>
-      </div>
-    </Link>
+      <Link
+        to={`/user/${poster.userId}`}
+        className={`flex flex-1 gap-2 ${className || ""}`}
+      >
+        <div>
+          <TooltipComp toolTipText={poster.username}>
+            <p className="line-clamp-1 text-lg font-semibold tracking-wide">
+              {poster.username}
+            </p>
+          </TooltipComp>
+          <TooltipComp toolTipText={`Posted: ${calcHowManyDaysAgo(postDate)}`}>
+            <p className="w-fit text-xs text-foreground/60">
+              {format(new Date(postDate), "LLLL/dd/yyyy")}
+            </p>
+          </TooltipComp>
+        </div>
+      </Link>
+      <ImageView image={viewedImage} handleClose={() => setViewedImage(null)} />
+    </div>
   );
 };
 
